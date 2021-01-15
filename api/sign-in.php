4a3bf92c197f8688b2bd86d0636093d5cc6ed8fa
Tarikul islam <tarikul.cse5.bu@gmail.com>
2021-01-15 23:09:46 +0600

    change some header in signin.php api

--- api/sign-in.php
+++ api/sign-in.php
@@ -1,7 +1,11 @@
 <?php
   // Headers
-  header('Content-Type: application/json');
-
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+  header("Access-Control-Allow-Methods: POST");
+  header("Access-Control-Max-Age: 3600");
+  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
   include_once '../config/Database.php';
   include_once '../models/Users.php';
 
